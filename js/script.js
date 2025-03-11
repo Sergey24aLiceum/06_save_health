89d5ffe56d9774477efc216864d7a91cfe931a45
Sergey24aLiceum <24a_ksv@liceum.ztu.edu.ua>
2025-03-11 12:53:13 +0200

    Налаштовано функціонал кнопок

--- js/script.js
+++ js/script.js
@@ -38,3 +38,25 @@ document.getElementById("btn-byu-tablets").addEventListener("click", () => {
   );
   document.getElementById("btn_health_wishes").style.display = "inline-block";
 });
+
+let galleryImage = 1
+
+document.getElementById("main-image").setAttribute("src", `images/gallery/${galleryImage}.jpg`);
+
+document.getElementById("right-arrow").addEventListener("click", () => {
+  galleryImage++
+  console.log(galleryImage)
+  
+  if (galleryImage = 4) galleryImage = 1
+
+  document.getElementById("main-image").setAttribute("src", `images/gallery/${galleryImage}.jpg`);
+})
+
+document.getElementById("left-image").addEventListener("click", () => {
+  galleryImage++
+  console.log(galleryImage)
+  
+  if (galleryImage == 4) galleryImage = 1
+
+  document.getElementById("main-image").setAttribute("src", `images/gallery/${galleryImage}.jpg`);
+})
