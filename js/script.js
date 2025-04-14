191c7d20838951f8540f09bab82cf825a7a58e66
Sergey24aLiceum <24a_ksv@liceum.ztu.edu.ua>
2025-04-14 08:18:54 +0300

    Зовнішньо деталізовано сайт

--- js/script.js
+++ js/script.js
@@ -74,76 +74,33 @@ document.getElementById("left-arrow").addEventListener("click", () => {
     .setAttribute("src", `img/gallery/${arrayOfImages[galleryImage - 1]}`);
 });
 
-const arrayOfVitamins = [
-  {
-    id: "001",
-    title: "Вітамін E",
-    photo: "vitamin-e.png",
-    description: "Вітамін E — група жиророзчинних біологічно активних сполук (токофероли та токотрієноли), що проявляють антиоксидантні властивості.",
-    rating: "4",
-    type: "жиророзчинних",
-  },
-  {
-    id: "002",
-    title: "Вітамін С",
-    photo: "vitamin-c.png",
-    description: "Аскорбíнова кислотá — відносно проста органічна кислота.",
-    rating: "3",
-    type: "аскорбінова кислота",
-  },
-  {
-    id: "003",
-    title: "Вітамін D ",
-    photo: "vitamin-d.png",
-    description: "Вітамі́н D має кілька форм. Їх називають кальцифероли і представлені вони переважно у вигляді двох речовин: ергокальциферолу (вітаміну D2), що надходить із дріжджів, та холекальциферолу (вітаміну D3), який отримано із тканин тварин.",
-    rating: "5",
-    type: "жиророзчинний",
-  },
-  {
-    id: "004",
-    title: "Вітамін B3",
-    photo: "molecule.png",
-    description: "Вітамін B3— розчинний у воді вітамін; необхідний для багатьох реакцій окиснення у живих клітинах.",
-    rating: "5",
-    type: "Нікоти́нова кислота́",
-  },
-  {
-    id: 5,
-    title: "Омега-3",
-    photo: "omega-3.png",
-    description: "Омега-3 - це поліненасичені жирні кислоти належать до родини ненасичених жирних кислот.",
-    rating: "3",
-    type: "тригліцеридів",
-  },
-  {
-    id: 6,
-    title: "Вітамін B5",
-    photo: "b5.png",
-    description: "Вітамін B5 — антидерматитний фактор.",
-    rating: "3",
-    type: "Пантоте́нова кислота́",
-  },
-];
+fetch("js/vitamins.json")
+        .then(response => response.json())
+        .then(data =>{
+          data.forEach((item,index) =>{
+        //console.log("елемент №",index.item)
 
-//console.log(arrayOfVitamins)
+        let divVitamin = document.createElement('div')
+        divVitamin.classList.add('vitamin')
+        
+        divVitamin.innerHTML = `
+                <p>${item.id}</p>
+                <h3>${item.title}</h3>
+                <hr>
+                <img src="img/vitamins/${item.photo}" alt="Картинка для ${item.title}" onerror="this.onerror = null; this.src="img/vitamins/defaut.png" ;>
+                <p>${item.description}</p>
 
-arrayOfVitamins.forEach((item, index) => {
-  //console.log("елемент №",index,item)
+                <div>
+                <img src="img/vitamins/${item.schema}" alt="" onerror = null; this.sc="img/vitamins/default_schema.png";>
+                <p>${"💚". repeat(item.rating)+'🤍'.repeat(5-item.rating)}</p>
+                <p>${item.type}</p>
+                </div>
+                `
 
-  let divVitamin = document.createElement("div");
-  divVitamin.classList.add("vitamin");
+        document.getElementById("p-vitamins").appendChild(divVitamin)
+      })
 
-  document.getElementById("p-vitamins").appendChild(divVitamin)
-
-  divVitamin.innerHTML = `
-          <p>${item.id}</p>
-        <h3>${item.title}</h3>
-          <hr>
-          <img src="img/vitamins/${item.photo}" alt="">
-          <p>${item.description}</p>
-          <div>
-          <p>${"💚".repeat(item.rating)+"🤍".repeat(5-item.rating)}</p>
-          <p>${item.type}</p>
-          </div>
-        `;
-});
+    })
+    .catch(error => {
+      console.error('Помилка при завантаженні JSON:', error);
+    });
